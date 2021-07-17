56369b68fef8ebfa304f583877150ec629c4a96a
midia-lima <midialp2@gmail.com>
2021-07-17 12:03:18 -0300

    Aula #3

---
+++ primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meu-primeiro',
+  templateUrl: './meu-primeiro.component.html'
+})
+
+export class MeuPrimeiroComponent {}
