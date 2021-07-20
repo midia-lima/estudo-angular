d572eeabc2fc489ee6dbb192e0feaa056580f560
midia-lima <midialp2@gmail.com>
2021-07-19 22:02:40 -0300

    #7 e #8introdução a serviços e injeção de dependencia

--- primeiro-projeto/src/app/cursos/cursos.component.ts
+++ primeiro-projeto/src/app/cursos/cursos.component.ts
@@ -1,8 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import {  CursosService } from './cursos.service'
 
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
   styleUrls: ['./cursos.component.css']
 })
-export class CursosComponent  {}
+export class CursosComponent  {
+
+  nomePortal: string;
+  cursos: string[]
+
+  constructor(public cursosService: CursosService) {
+    this.nomePortal = "http://loiane.training.com.br"
+
+    this.cursos = this.cursosService.getCursos();
+
+    //var service = new CursosService();
+
+
+
+
+
+  }
+
+
+
+}
