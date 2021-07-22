3c1bb66a1d3c5a9c5066b711f712f340c569a15f
midia-lima <midialp2@gmail.com>
2021-07-21 22:03:33 -0300

    #8, #9, #10, #11

---
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css']
+})
+
+export class DataBindingComponent  {
+
+  url: string = 'http://loiane.com';
+  cursoAngular: boolean = true;
+  urlImagem = 'https://picsum.photos/seed/picsum/500/300'
+  valorAtual: string = ''
+  valorSalvo = '';
+  isMouseOver: boolean = false;
+
+  getValor(){
+    return 1
+  }
+
+  getCurtirCurso(){
+    return true;
+  }
+
+  botaoClicado(){
+    alert('botao clicado');
+  }
+
+  onKeyUp(evento: KeyboardEvent){
+    this.valorAtual = ((<HTMLInputElement>evento.target).value)
+  }
+
+  salvarValor(valor){
+    this.valorSalvo = valor
+  }
+
+  onMouseOverOut(){
+    this.isMouseOver = !this.isMouseOver;
+
+  }
+
+
+
+
+
+
+  constructor() { }
+
+
+}
